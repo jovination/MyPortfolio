9e95c37f38c9369d5293899e2147a81cd0b3e811
jovination <jovination791@gmail.com>
2024-02-13 22:02:10 +0300

    few changes in js for icon animation

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
-function colorize() {
-    var hue = Math.random() * 360;
-    return "HSL(" + hue + ",100%,50%)";
-  }
-  
-  $("#sonar-wave").on("webkitAnimationIteration oanimationiteration animationiteration", function(){
-    $(this).css("background-color", colorize());
+// Use let instead of var for block scope
+let hue;
+
+// Use arrow functions for cleaner syntax
+const triggerAnimation = (event, element) => {
+  event.stopPropagation();
+  element.classList.add('active');
+  setTimeout(() => {
+    element.classList.remove('active');
+  }, 1300);
+}
+
+const colorize = () => {
+  hue = Math.random() * 360;
+  return `HSL(${hue},100%,50%)`;
+}
+
+$("#sonar-wave").on("webkitAnimationIteration oanimationiteration animationiteration", function(){
+  $(this).css("background-color", colorize());
+});
+
+new SimpleBar(document.querySelector('.wrapper'));
+
+const icons = document.querySelectorAll('.icon');
+
+icons.forEach((icon) => {
+  const sonarEffect = document.createElement('div');
+  sonarEffect.classList.add('sonar-effect');
+  icon.appendChild(sonarEffect);
+
+  icon.addEventListener('mouseenter', () => {
+    const effect = icon.querySelector('.sonar-effect');
+    effect.style.animation = 'none';
+    void effect.offsetWidth;
+    effect.style.animation = 'sonar 1s ease-out';
   });
-  
-  new SimpleBar(document.querySelector('.wrapper'));
-   // Get all the icon elements
-   const icons = document.querySelectorAll('.icon');
+});
 
-   // Add a sonar effect to each icon on hover
-   icons.forEach((icon) => {
-       const sonarEffect = document.createElement('div');
-       sonarEffect.classList.add('sonar-effect');
-       icon.appendChild(sonarEffect);
-   
-       icon.addEventListener('mouseenter', () => {
-           const effect = icon.querySelector('.sonar-effect');
-           effect.style.animation = 'none';
-           void effect.offsetWidth; // Trigger reflow to restart animation
-           effect.style.animation = 'sonar 1s ease-out';
-       });
-   });
-   
-   //close popup window
-   function openPopup() {
-    document.getElementById("contactPopup").style.display = "block";
-  }
+function openPopup() {
+  document.getElementById("contactPopup").style.display = "block";
+}
 
-  function closePopup() {
-    document.getElementById("contactPopup").style.display = "none";
-  }
-  function openLink() {
-    var link = document.querySelector('.btn_link').href;
-    window.open(link, '_blank');
+function closePopup() {
+  document.getElementById("contactPopup").style.display = "none";
+}
+
+function openLink() {
+  var link = document.querySelector('.btn_link').href;
+  window.open(link, '_blank');
 }
